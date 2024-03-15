425b58a19f585f8b26a9ccc347e5a138ed09e95d
Glauciane Figueiredo20 <104854431+glaucianepeligrini18@users.noreply.github.com>
2024-03-15 10:07:44 -0300

    .

--- main.js
+++ main.js
@@ -9,4 +9,10 @@ for(let i=0;i <botoes.length;i++){
 
         botoes[i].classList.add("ativo");
     }
-}
+}  <div class="abas-textos">
+<div class="aba-conteudo">
+    <h3 class="aba-conteudo-titulo-principal">Estudar 4 cursos na Alura</h3>
+    <h4 class="aba-conteudo-titulo-secundario">Tempo para completar o objetivo</h4>
+</div>
+</div>
+
